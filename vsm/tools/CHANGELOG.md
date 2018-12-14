45897988f619a41ebad0e3c4724606a205fb01e5
Edward Haletky <elh@astroarch.com>
2018-12-14 09:48:29 -0600

    Updates for Horizon in vsm_favorites.sh

---
+++ vsm/tools/CHANGELOG.md
@@ -0,0 +1,1 @@
+12/14/19 - Updated vsm_favorites.sh with latest Horizon
--- vsm/tools/vsm_favorites.sh
+++ vsm/tools/vsm_favorites.sh
@@ -25,5 +25,5 @@ $vsm -y --debug --oauth --fav Datacenter_Cloud_Infrastructure_VMware_vSphere_6_5
 $vsm -y --debug --oauth --fav Datacenter_Cloud_Infrastructure_VMware_vSphere_6_0_Enterprise_Plus
 $vsm -y --debug --oauth --fav Datacenter_Cloud_Infrastructure_VMware_vSphere_5_5_Enterprise_Plus
 $vsm -y --debug --oauth --fav Networking_Security_VMware_NSX_T_Data_Center_2_x_VMware_NSX_Data_Center_Enterprise_Plus
-$vsm -y --debug --oauth --fav Desktop_End_User_Computing_VMware_Horizon_7_6_Horizon_7_Enterprise_Edition
+$vsm -y --debug --oauth --fav Desktop_End_User_Computing_VMware_Horizon_7_7_Horizon_7_Enterprise_Edition
 $vsm -y --debug --oauth --fav Infrastructure_Operations_Management_VMware_vRealize_Suite_2018_Enterprise
